fc36db71031ebb1706caae35034a51aabf3b1114
danicoll0 <danicoll@ucm.es>
2025-10-19 19:43:11 +0200

    Creación pantalla principal y estructuración del proyecto.

--- not&notion/app/src/main/java/es/fdi/ucm/pad/notnotion/ui/main/MainActivity.java
+++ not&notion/app/src/main/java/es/fdi/ucm/pad/notnotion/ui/main/MainActivity.java
@@ -1,4 +1,4 @@
-package es.fdi.ucm.pad.notnotion;
+package es.fdi.ucm.pad.notnotion.ui.main;
 
 import android.os.Bundle;
 
@@ -7,18 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import es.fdi.ucm.pad.notnotion.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;      // ✅ Variable global
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Ajuste para pantallas Edge-to-Edge
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        /*
+        Esto para hacer que el recyclerView muestre los items de 3 en 3
+        recyclerView = findViewById(R.id.recyclerItems);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+         */
     }
 }
