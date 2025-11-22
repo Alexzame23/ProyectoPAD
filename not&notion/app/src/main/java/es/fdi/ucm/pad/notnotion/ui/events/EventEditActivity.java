16a085e2ec656b76ec47e686c92f72c26cc35408
danicoll0 <danicoll@ucm.es>
2025-11-22 11:28:51 +0100

    Añadido boton return en profile_activity y en edit_note.xml

--- not&notion/app/src/main/java/es/fdi/ucm/pad/notnotion/ui/events/EventEditActivity.java
+++ not&notion/app/src/main/java/es/fdi/ucm/pad/notnotion/ui/events/EventEditActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -106,6 +107,10 @@ public class EventEditActivity extends AppCompatActivity {
 
         // --- BOTÓN GUARDAR ---
         btnSave.setOnClickListener(v -> saveEvent());
+
+        // --- BOTÓN RETURN ---
+        ImageButton btnGoBack = findViewById(R.id.btnGoBackCalendar);
+        btnGoBack.setOnClickListener(v -> finish());
     }
 
     // ============================
--- not&notion/app/src/main/java/es/fdi/ucm/pad/notnotion/ui/profile/ProfileActivity.java
+++ not&notion/app/src/main/java/es/fdi/ucm/pad/notnotion/ui/profile/ProfileActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -64,6 +65,9 @@ public class ProfileActivity extends AppCompatActivity {
         //      LOGOUT
         // ---------------------------------------------------------
         btnLogout.setOnClickListener(v -> logout());
+
+        ImageButton btnGoBack = findViewById(R.id.btnGoBackCalendar);
+        btnGoBack.setOnClickListener(v -> finish());
     }
 
 
